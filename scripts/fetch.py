1dde156e94b2a3c82e7a1321f533b33d6250bcd7
Simon Liska <40433119+simonliska@users.noreply.github.com>
2024-08-18 12:40:10 +0200

    Added x_gm_label

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -1,8 +1,13 @@
-from imap_tools import MailBox
+# https://pypi.org/project/imap-tools/
+from imap_tools import MailBox, OR
+
+EMAIL_ADDRESS = 'YOUR_EMAIL_ADDRESS'
+EMAIL_PASSWORD = 'YOUR_SECRET'
+LABEL = 'YOUR_LABEL' 
 
 # get all attachments from INBOX and save them to files
-with MailBox('imap.gmail.com').login('YOUR_EMAIL_ADDRESS', 'YOUR_SECRET', 'INBOX') as mailbox:
-    for msg in mailbox.fetch('UNSEEN'):
+with MailBox('imap.gmail.com').login(EMAIL_ADDRESS, EMAIL_PASSWORD, 'INBOX') as mailbox:
+    for msg in mailbox.fetch(OR(seen=False, x_gm_label=LABEL)):
         for att in msg.attachments:
             print(att.filename, att.content_type)
             with open('/var/cache/raw/{}'.format(att.filename), 'wb') as f:
